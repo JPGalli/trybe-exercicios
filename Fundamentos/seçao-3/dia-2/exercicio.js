25fab9730a75007c9fb17da58d46fe1e3c7af258
Joao Pedro Galli <joaogalli10@hotmail.com>
2023-03-29 18:44:30 -0300

    mais um exercicio

--- Fundamentos/seçao-3/dia-2/exercicio.js
+++ Fundamentos/seçao-3/dia-2/exercicio.js
@@ -2,8 +2,13 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let media;
 
 for(let indice = 0; indice < numbers.length; indice += 1){
     sum += numbers[indice];
+    media = sum / (indice + 1);
 }
   console.log(sum);
+  console.log(media);
+  
+  
