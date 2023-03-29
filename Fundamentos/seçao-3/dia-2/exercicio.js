96d7267bb7caa33b4ff7f13762a0f66eb28d6d86
Joao Pedro Galli <joaogalli10@hotmail.com>
2023-03-29 18:37:48 -0300

    realizando exercicios

---
+++ Fundamentos/seçao-3/dia-2/exercicio.js
@@ -0,0 +1,9 @@
+// começando
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for(let indice = 0; indice < numbers.length; indice += 1){
+    sum += numbers[indice];
+}
+  console.log(sum);
