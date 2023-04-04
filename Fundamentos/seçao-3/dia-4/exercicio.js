655ff7c462af6d1a07dd7ed8830661968f4d017e
Joao Pedro Galli <joaogalli10@hotmail.com>
2023-04-04 15:44:21 -0300

    Estudos dia 5 java

--- Fundamentos/seçao-3/dia-4/exercicio.js
+++ Fundamentos/seçao-3/dia-4/exercicio.js
@@ -2,10 +2,29 @@ let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim'  
+  recorrente: 'Sim'
 };
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+  
+//   console.log("Bem-vinda,",info.personagem);
+//   console.log();
+// for(let all in info){
+//   console.log(all)  
+// }
+// for(let key in info){
+//       console.log(info[key])  
+//     }
 
-  console.log("Bem-vinda,",info.personagem);
-for(let all in info){
-  console.log(all)  
+for(let key in info){
+  if(key === 'recorrente' & info[key] === 'sim' && info2[key] === 'sim'){
+    console.log("Ambos recorrentes //"); 
+  }
+  else{
+    console.log(info[key] + ' e ' + info2[key]);
+  }
 }
