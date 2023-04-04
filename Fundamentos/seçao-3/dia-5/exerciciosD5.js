547a1c57b2186a91b5f860e4dce11da1b2a973c0
Joao Pedro Galli <joaogalli10@hotmail.com>
2023-04-04 15:49:11 -0300

    Fixando aprendizados

---
+++ Fundamentos/seçao-3/dia-5/exerciciosD5.js
@@ -0,0 +1,11 @@
+// function soma (num1,num2){
+//   return num1 + num2;  
+// }
+
+// console.log(soma(2, 2));
+
+const soma = function (num1,num2){
+  return num1 + num2;  
+}
+
+console.log(soma(2,2))
